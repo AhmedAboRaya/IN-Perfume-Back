4c9bcaaf6617501eb986ac6e80fc00e85b2769ce
Ahmed Abo-Raya <88713925+AhmedAboRaya@users.noreply.github.com>
2025-05-29 02:18:28 +0300

    update error.js file

--- middleware/error.js
+++ middleware/error.js
@@ -1,11 +1,17 @@
-// utils/errorHandler.js
-class ErrorHandler extends Error {
-  constructor(message, statusCode) {
-    super(message);
-    this.statusCode = statusCode;
-    this.isOperational = true;
-    Error.captureStackTrace(this, this.constructor);
-  }
-}
+const ErrorHandler = require("../utils/errorHandler");
 
-module.exports = ErrorHandler;
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal Server Error";
+
+  // Handle MongoDB CastError (e.g., invalid ObjectId)
+  if (err.name === "CastError") {
+    const message = `Resource not found. Invalid: ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
+};
